6d3b1707cbb8fb8314e86f6156ca265426c4b0a4
ajaykumar0021 <45794917+ajaykumar0021@users.noreply.github.com>
2019-08-09 00:43:24 +0530

    Add files via upload

---
+++ coffee.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+
+{   
+    
+int X;
+  
+int P;
+    
+cin>>X;
+    
+cin>>P;
+    
+float y=(float)90 / 100;
+    
+int sum=0;
+    
+while(X>1)
+   
+ {
+        
+sum=sum+X;
+        
+int k=X*y;
+        
+X=X-k;
+    
+}
+        
+cout<<sum<<endl;    
+return 0;
+
+}
